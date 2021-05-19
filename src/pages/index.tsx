743d6a864855a51bfaded069e1a3d5fc599d1c43
Cristiano Augusto <cristiano.asg@hotmail.com>
2021-05-19 22:10:40 +0100

    dynamic title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <head>
+        <title>Inicio | ig.news</title>
+      </head>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
