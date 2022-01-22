3d0f74d62ef5573c2f665640a458595a3dcc1918
Cristiano Augusto <cristiano.asg@hotmail.com>
2022-01-22 07:27:52 +0000

    listing posts on canvas

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { getPrismicClient } from '../../services/prismic';
-import styles from './styles.module.scss';
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic';
+
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,21 +27,14 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>05 de junho de 2022</time>
-            <strong> Creating a Monorepo with Learning & Yarn Workspaces</strong>
-            <p>Breve descrição do paragrafo</p>
-          </a>
-          <a href="#">
-            <time>05 de junho de 2022</time>
-            <strong> Creating a Monorepo with Learning & Yarn Workspaces</strong>
-            <p>Breve descrição do paragrafo</p>
-          </a>
-          <a href="#">
-            <time>05 de junho de 2022</time>
-            <strong> Creating a Monorepo with Learning & Yarn Workspaces</strong>
-            <p>Breve descrição do paragrafo</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+
         </div>
       </main>
     </>
@@ -37,16 +44,29 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
 
-  const response = await prismic.query([
+  const response = await prismic.query<any>([
     Prismic.predicates.at('document.type', 'post')
   ], {
     fetch: ['publication.title', 'publication.content'],
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
